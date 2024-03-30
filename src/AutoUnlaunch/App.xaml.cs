094f5de76e9838ced50a96e66f67698b6ace24b2
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-03-30 10:50:59 -0400

    Use .NET hosting for DI.

--- src/AutoUnlaunch/App.xaml.cs
+++ src/AutoUnlaunch/App.xaml.cs
@@ -1,20 +1,36 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Xaml;
 
 namespace MrCapitalQ.AutoUnlaunch;
 
 public partial class App : Application
 {
-    public App()
+    public App(IServiceProvider services)
     {
         InitializeComponent();
+        Services = services;
     }
 
     public static new App Current => (App)Application.Current;
+    public IServiceProvider Services { get; }
     public Window? Window { get; protected set; }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
-        Window = new MainWindow();
+        Window = Services.GetRequiredService<MainWindow>();
         Window.Activate();
+        Window.Closed += Window_Closed;
+    }
+
+    private void Window_Closed(object sender, WindowEventArgs args)
+    {
+        if (sender is Window window)
+            window.Closed -= Window_Closed;
+
+        Window = null;
+
+        var hostApplicationLifetime = Services.GetRequiredService<IHostApplicationLifetime>();
+        hostApplicationLifetime.StopApplication();
     }
 }
---
+++ src/AutoUnlaunch/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MrCapitalQ.AutoUnlaunch;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHostedService<WindowsAppHostedService<App>>();
+
+builder.Services.AddSingleton<App>();
+builder.Services.AddSingleton<MainWindow>();
+
+var host = builder.Build();
+host.Run();
