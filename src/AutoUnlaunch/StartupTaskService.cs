399f32007d944bbfcb27edb5609dbd8ba61ff4ce
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-03-30 11:03:56 -0400

    Configure app to run at startup.

---
+++ src/AutoUnlaunch/StartupTaskService.cs
@@ -0,0 +1,23 @@
+﻿using Windows.ApplicationModel;
+
+namespace MrCapitalQ.AutoUnlaunch;
+
+internal class StartupTaskService
+{
+    private const string StartupTaskId = "b289bd30-d771-44a6-ac8c-1e7c0a5b3d17";
+
+    public async Task<StartupTaskState> GetStartupStateAsync()
+    {
+        var startupTask = await StartupTask.GetAsync(StartupTaskId);
+        return startupTask.State;
+    }
+
+    public async Task SetStartupStateAsync(bool isEnabled)
+    {
+        var startupTask = await StartupTask.GetAsync(StartupTaskId);
+        if (isEnabled)
+            await startupTask.RequestEnableAsync();
+        else
+            startupTask.Disable();
+    }
+}
