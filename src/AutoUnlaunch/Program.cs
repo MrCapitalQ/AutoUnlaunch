4856d4fb489c02f761cbe36b5c6c495721d9995d
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-03-30 10:56:01 -0400

    Make app single instance only.

--- src/AutoUnlaunch/Program.cs
+++ src/AutoUnlaunch/Program.cs
@@ -1,6 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Windows.AppLifecycle;
 using MrCapitalQ.AutoUnlaunch;
+
+var keyInstance = AppInstance.FindOrRegisterForKey("fa1bd88a-766a-45eb-be78-e9fc27e995bf");
+if (!keyInstance.IsCurrent)
+{
+    var activationArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+    await keyInstance.RedirectActivationToAsync(activationArgs);
+    return;
+}
 
 var builder = Host.CreateApplicationBuilder(args);
 
