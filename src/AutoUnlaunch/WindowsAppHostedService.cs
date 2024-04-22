dd71ab5019f1aaaa4311722c8336968acaec41c8
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-04-22 08:55:13 -0400

    Use .NET hosting for DI. (#2)

---
+++ src/AutoUnlaunch/WindowsAppHostedService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+
+namespace MrCapitalQ.AutoUnlaunch;
+
+internal class WindowsAppHostedService<TApplication> : IHostedService
+    where TApplication : Application
+{
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly IServiceProvider _serviceProvider;
+
+    public WindowsAppHostedService(IHostApplicationLifetime hostApplicationLifetime, IServiceProvider serviceProvider)
+    {
+        _hostApplicationLifetime = hostApplicationLifetime;
+        _serviceProvider = serviceProvider;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        var thread = new Thread(Main);
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    private void Main()
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Application.Start(p =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _serviceProvider.GetRequiredService<TApplication>();
+        });
+        _hostApplicationLifetime.StopApplication();
+    }
+}
