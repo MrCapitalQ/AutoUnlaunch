f839dc096da8413557bbf49b6d450dd4ee1926ad
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-04-23 11:13:59 -0400

    Add page navigation.

---
+++ src/AutoUnlaunch/Shared/NavigateMessage.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml.Media.Animation;
+
+namespace MrCapitalQ.AutoUnlaunch.Shared;
+
+internal record NavigateMessage(Type SourcePageType, object? Parameter = null);
+
+internal record SlideNavigateMessage(Type SourcePageType,
+    SlideNavigationTransitionEffect SlideEffect,
+    object? Parameter = null)
+    : NavigateMessage(SourcePageType, Parameter);
