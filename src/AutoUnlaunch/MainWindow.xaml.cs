e63c3f7f57c2df842067fabeea60a98ba5453b25
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-04-23 07:52:18 -0400

    Add custom app icon. (#3)

--- src/AutoUnlaunch/MainWindow.xaml.cs
+++ src/AutoUnlaunch/MainWindow.xaml.cs
@@ -1,12 +1,16 @@
 using Microsoft.UI.Xaml;
+using Windows.ApplicationModel;
+using WinUIEx;
 
 namespace MrCapitalQ.AutoUnlaunch;
 
-public sealed partial class MainWindow : Window
+public sealed partial class MainWindow : WindowEx
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        Title = Package.Current.DisplayName;
     }
 
     private void myButton_Click(object sender, RoutedEventArgs e)
