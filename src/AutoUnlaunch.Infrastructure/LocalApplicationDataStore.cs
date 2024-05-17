e1194a279dd5b17f0edcb65b1672c0b97406f27e
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-05-17 08:35:40 -0400

    Cache settings in memory. (#25)

--- src/AutoUnlaunch.Infrastructure/LocalApplicationDataStore.cs
+++ src/AutoUnlaunch.Infrastructure/LocalApplicationDataStore.cs
@@ -1,12 +1,25 @@
-﻿using MrCapitalQ.AutoUnlaunch.Core.AppData;
+﻿using Microsoft.Extensions.Caching.Memory;
+using MrCapitalQ.AutoUnlaunch.Core.AppData;
 using Windows.Storage;
 
 namespace MrCapitalQ.AutoUnlaunch.Infrastructure;
 
-public class LocalApplicationDataStore : IApplicationDataStore
+public class LocalApplicationDataStore(IMemoryCache cache) : IApplicationDataStore
 {
     private readonly ApplicationDataContainer _localSettings = ApplicationData.Current.LocalSettings;
+    private readonly IMemoryCache _cache = cache;
 
-    public object? GetValue(string key) => _localSettings.Values[key];
-    public void SetValue(string key, object? value) => _localSettings.Values[key] = value;
+    public object? GetValue(string key)
+    {
+        if (_cache.TryGetValue(key, out var value))
+            return value;
+
+        return _cache.Set(key, _localSettings.Values[key]);
+    }
+
+    public void SetValue(string key, object? value)
+    {
+        _cache.Set(key, value);
+        _localSettings.Values[key] = value;
+    }
 }
