f83a9136a95d18549d5e859c08cad387b056e171
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-05-17 11:08:10 -0400

    Add taskbar tray icon and use efficiency mode. (#26)

--- src/AutoUnlaunch/LIfetimeWindow.xaml.cs
+++ src/AutoUnlaunch/LIfetimeWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
 using MrCapitalQ.AutoUnlaunch.Shared;
 using System.Diagnostics.CodeAnalysis;
 
@@ -7,5 +8,16 @@ namespace MrCapitalQ.AutoUnlaunch;
 [ExcludeFromCodeCoverage(Justification = ExcludeFromCoverageJustifications.RequiresUIThread)]
 public sealed partial class LifetimeWindow : Window
 {
-    public LifetimeWindow() => InitializeComponent();
+    public LifetimeWindow()
+    {
+        InitializeComponent();
+
+        TrayIcon.ToolTipText = Windows.ApplicationModel.Package.Current.DisplayName;
+    }
+
+    private void OpenApplicationCommand_ExecuteRequested(XamlUICommand sender, ExecuteRequestedEventArgs args)
+        => App.Current.ShowMainWindow();
+
+    private void ExitApplicationCommand_ExecuteRequested(XamlUICommand sender, ExecuteRequestedEventArgs args)
+        => App.Current.Exit();
 }
